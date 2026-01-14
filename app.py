23a594e65584cf1cb96cf5ea31772cb680985b99
COZYkrish <cozykrish2916@gmail.com>
2026-01-14 14:49:00 +0530

    implement short code generator

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 import json
 import os
+import random
+import string
+
 app = Flask(__name__)
 
 DATA_FILE = "data/urls.json"
@@ -19,6 +22,10 @@ def save_urls(urls):
     with open(DATA_FILE, "w") as file:
         json.dump(urls, file, indent=4)
 
+def generate_short_code(length=6):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
 
 
 @app.route("/")
@@ -27,3 +34,4 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
