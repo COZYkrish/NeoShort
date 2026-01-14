ba0c0ee4417ca04d20885061068f9b05c051de28
COZYkrish <cozykrish2916@gmail.com>
2026-01-14 14:43:28 +0530

    add json based storage for urls

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template
-
+import json
+import os
 app = Flask(__name__)
+
+DATA_FILE = "data/urls.json"
+
+def load_urls():
+    if not os.path.exists(DATA_FILE):
+        return {}
+
+    with open(DATA_FILE, "r") as file:
+        try:
+            return json.load(file)
+        except json.JSONDecodeError:
+            return {}
+
+def save_urls(urls):
+    with open(DATA_FILE, "w") as file:
+        json.dump(urls, file, indent=4)
+
+
 
 @app.route("/")
 def home():
