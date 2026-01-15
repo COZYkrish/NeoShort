6b716f963432d3c98f16b23e9a396ae8015f77ab
COZYkrish <cozykrish2916@gmail.com>
2026-01-15 20:30:12 +0530

    add toast notification for user feedback

--- static/js/main.js
+++ static/js/main.js
@@ -7,3 +7,12 @@ if (copyBtn) {
     showToast("Copied to clipboard!");
   });
 }
+function showToast(message) {
+  const toast = document.getElementById("toast");
+  toast.innerText = message;
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 2000);
+}
